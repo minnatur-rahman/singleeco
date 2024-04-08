841309da6b6a734b68f48784f7d2924f9efc3956
millatur <minnatur7373@gmail.com>
2024-04-08 16:22:15 +0600

    udeyhwetgd

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -7,5 +7,14 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('admin.allcategory');
+    }
+
+    public function addcategory()
+    {
+
+    }
 }
+
---
+++ resources/views/admin/allcategory.blade.php
@@ -0,0 +1,4 @@
+@extends('admin.layouts.template')
+@section('content')
+<h1>This is category all</h1>
+@endsection
