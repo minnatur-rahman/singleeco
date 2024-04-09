f1a551f658796d3e401084c3c2262b4b6a43d56a
millatur <minnatur7373@gmail.com>
2024-04-09 08:10:55 +0600

    yew7yeuws

--- resources/views/admin/allproduct.blade.php
+++ resources/views/admin/allproduct.blade.php
@@ -5,17 +5,17 @@ All Products - Single Ecom
 @section('content')
     <!-- Bootstrap Table with Header - Light -->
     <div class="container-xxl flex-grow-1 container-p-y">
-        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Pages/</span>All Sub Category</h4>
+        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Pages/</span>All Product</h4>
         <div class="card">
-            <h5 class="card-header">Available Sub Category Information</h5>
+            <h5 class="card-header">Available Products Information</h5>
             <div class="table-responsive text-nowrap">
                 <table class="table">
                     <thead class="table-light">
                         <tr>
                             <th>Id</th>
-                            <th>Sub Category Name</th>
-                            <th>Category</th>
-                            <th>Products</th>
+                            <th>Product Name</th>
+                            <th>Image</th>
+                            <th>Price</th>
                             <th>Actions</th>
                         </tr>
                     </thead>
@@ -23,7 +23,7 @@ All Products - Single Ecom
                         <tr>
                             <td>1</td>
                             <td>Fan</td>
-                            <td>Electronics</td>
+                            <td></td>
                             <td>90</td>
                             <td>
                                 <a href="" class="btn btn-primary">Edit</a>
