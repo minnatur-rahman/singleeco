d55180d1a9ff505b9a3ae9b771737717d502b406
millatur <minnatur7373@gmail.com>
2024-04-10 11:55:21 +0600

    ehdfueh

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\Product;
 use App\Models\Subcategory;
 use Illuminate\Http\Request;
 
@@ -45,5 +46,20 @@ class ProductController extends Controller
 
         $category_name = Category::where('id',$category_id)->value('category_name');
         $subcategory_name = Subcategory::where('id',$subcategory_id)->value('subcategory_name');
+
+        Product::insert([
+            'product_name' => $request->product_name,
+            'product_short_des' => $request->product_short_des,
+            'product_long_des' => $request->product_long_des,
+            'price' => $request->price,
+            'product_category_name' => $category_name,
+            'product_subcategory_name' => $subcategory_name,
+            'product_category_id' => $request->product_category_id,
+            'product_subcategory_id' => $request->product_subcategory_id,
+            'product_img' =>  $img_url,
+            'quantity' => $request->quantity,
+            'subcategory_name' => $request->subcategory_name,
+            'slug' => strtolower(str_replace('','-',$request->subcategory_name)),
+        ]);
     }
 }
