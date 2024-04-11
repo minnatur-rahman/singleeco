2b019d4f9d13ee421c45d6047d395c1ce60c2cb2
millatur <minnatur7373@gmail.com>
2024-04-11 11:19:53 +0600

    hdushdu

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
-    //
+    public function CategoryPage()
+    {
+        return view('user_template.category');
+    }
 }
