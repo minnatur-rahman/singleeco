9603048798bcd1d13d0c5da50f0ad3f3bb8300cc
millatur <minnatur7373@gmail.com>
2024-04-11 06:26:23 +0600

    udhusihd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,10 +1,14 @@
-<?php
+ <?php
 
 namespace App\Http\Controllers;
+
 
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function Index()
+    {
+        return view('user_template.layouts.template');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,23 +7,19 @@ use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\SubcategoryController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('user_template.layouts.template');
+
+// Route::get('/', function () {
+//     return view('user_template.layouts.template');
+// });
+
+Route::controller(HomeController::class)->group(function(){
+    Route::get('/', 'Index')->name('home');
 });
 
 Route::get('/dashboard', function () {
