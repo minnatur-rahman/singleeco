d060c405d4a82c4d159763b5c09e5f608b2e60a0
millatur <minnatur7373@gmail.com>
2024-04-12 21:29:05 +0600

    dhwsysg

--- resources/views/user_template/product.blade.php
+++ resources/views/user_template/product.blade.php
@@ -1,4 +1,13 @@
 @extends('user_template.layouts.template')
 @section('main-content')
-<h1>Single Product page</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4">
+              left
+        </div>
+        <div class="col-lg-8">
+            right
+        </div>
+    </div>
+</div>
 @endsection
