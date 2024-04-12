9c13eb3c605d5d1048b8e9c1739abdf6db735cc9
millatur <minnatur7373@gmail.com>
2024-04-12 21:49:56 +0600

    udhshgd

--- resources/views/user_template/product.blade.php
+++ resources/views/user_template/product.blade.php
@@ -3,10 +3,20 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-4">
-              left
+            <div class="box_main">
+                <div class="tshirt_img"><img src="images/tshirt-img.png"></div>
+            </div>
         </div>
         <div class="col-lg-8">
-            right
+            <div class="box_main">
+                <div class="product-info">
+                    <h4 class="shirt_text">Man T -shirt</h4>
+                    <p class="price_text">Price  <span style="color: #262626;">$ 30</span></p>
+                </div>
+                <div class="my-3 product-details">
+                    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, labore similique quas libero facilis possimus, doloribus minus maxime natus cumque quasi cupiditate voluptas aliquam, esse autem eligendi illo ea tenetur!</p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
